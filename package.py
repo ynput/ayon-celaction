42599a89b386648ebe19f7084afb03eeefa1e0f7
Jakub Trllo <43494761+iLLiCiTiT@users.noreply.github.com>
2026-02-25 17:41:59 +0100

    bump required ayon core

--- package.py
+++ package.py
@@ -7,7 +7,7 @@ project_can_override_addon_version = True
 
 ayon_server_version = ">=1.1.2"
 ayon_required_addons = {
-    "core": ">0.3.2",
+    "core": ">=1.8.0",
 }
 ayon_compatible_addons = {
     "applications": ">=0.2.0",
